8aa29b6ceb2373343600f25876f4713503e9dc62
dgohdz <diegohmailbox@gmail.com>
2021-05-16 23:39:08 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,9 +15,7 @@
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
-// a) Create a test with expect statements for each of the variables provided.
-
-var num1 = 15
+/*var num1 = 15
 // Expected output: "15 is divisible by three"
 
 var num2 = 0
@@ -26,17 +24,52 @@ var num2 = 0
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
+//Write function named divideThree with the parameter/argument named num
+
+const divideThree = (num) => {
 
 
+
+    
+//Write if statment if num is divided by three it is strictly equal to zero.
+    if(num % 3 === 0){
+
+//return a string interpolation stating num is divisible by three
+        return `${num} is divisible by three`
+
+//write an else if statement of num divided by three is not equal to zero
+    } else if(num % 3 !==0){
+
+// return with string interpolation stating num is not divisible by three
+        return `${num} is not divisible by three`
+// input else statement to output something is wrong if there is an error.
+    } else {
+        return "Something is wrong"
+    }
+}
+
+// a) Create a test with expect statements for each of the variables provided.
+
+//write test with the method "divideThree" to return is divisible by three.
+describe("divideThree", () => {
+    test ("returns is divisible by three", () => {
+        expect(divideThree().toEqual("is divisible by three"))
+    })
+})
 
 
 // b) Create the function that makes the test pass.
-
+//write the function diviethree to return is divisible by three.
+divideThree = () => {
+    return "is divisible by three"
+} //Not sure why I can't pass the test.*/
 
 
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+    
+}
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -46,7 +79,14 @@ var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
-
+//Declare a function named capWords with a parameter named array.
+const capWords = (array) => {
+//create a variable for the new mapped array
+    let newArr = array.map(value => {
+//access the first index and capitalize the first letter for each word.
+        return value[0].toUpperCase()
+    })
+    //got stuck and had trouble finding the right instruction in the syllabus.
 
 
 
@@ -67,7 +107,15 @@ var vowelTester2 = "academy"
 var vowelTester3 = "challenge"
 // Expected output: 2
 
+//Declare function called vowelsIndex
+const vowelsIndex = (vowel) => {
+//use .split() to mae an array out of the strings.
+    let firstVowel = vowel.split(value => {
 
+    }
+}
+
+//I got stuck.
 
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. The built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: I'm honestly not sure. I'm guessing arrays.
 
-  Researched answer:
+  Researched answer: .map() parameters are value, indez and array.
 
 
 
 2. What is object destructuring?
 
-  Your answer:
+  Your answer: To break down in segments?
 
-  Researched answer:
+  Researched answer:It is taking variables and assigning them in a specific way.
 
 
 
 3. Why would you use the method super()?
 
-  Your answer:
+  Your answer: I dont remember but it has something to do with classes.
 
-  Researched answer:
+  Researched answer: super() passes information from the contructor.
 
 
 
 4. What is a React Fragment? Why would you use one?
 
-  Your answer:
+  Your answer: I don't remember
 
-  Researched answer:
+  Researched answer: It's what you use to next other components.
 
 
 
 5. What is JSX?
 
-  Your answer:
+  Your answer: Something that javascript uses to allow it to work within a browser?
 
-  Researched answer:
+  Researched answer: It's Javascripts version of HTML
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: moving code to a different location.
 
-  Researched answer:
+  Researched answer: Moving variables and functions to the top of the code.
 
 
 
@@ -58,8 +58,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. React props:
-2. React lifecycle methods:
-3. Logic/smart component:
-4. Display/dumb component:
-5. DOM Events:
+1. React props: Give the ability to pass down data.
+2. React lifecycle methods: There are 3 main phases that organize React.
+3. Logic/smart component:These are more complicated components.
+4. Display/dumb component: This is a more efficient component.
+5. DOM Events: Used to handle different elements in an HTML document.
